ff85116eda1911ed6a33a52e92c0bb05a4e40e86
Nicolas Borson <nicolasborson@hotmail.fr>
2020-02-04 11:21:56 +0100

    added menu effects

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import './css/Home.css'
-import louCardRB from '../assets/louCard.png'
+// import louCardRB from '../assets/louCard.png'
 import louCardWB from '../assets/LOGO BLANC.png'
-import louCardTop from '../assets/LOGO BLANC haut.png'
-import louCardBottom from '../assets/LOGO BLANC bas.png'
+// import louCardTop from '../assets/LOGO BLANC haut.png'
+// import louCardBottom from '../assets/LOGO BLANC bas.png'
 
 const Home = () => {
 
     const [start, setStart] = useState('logostart')
     const [shine, setShine] = useState('')
     const [isTop, setIsTop] = useState(true)
-    const [tagHeight, setTagHeight] = useState(484)
+    const [headHeight, setHeadHeight] = useState(484)
 
     useEffect(() => {
-        window.scroll(0, 0)
+        window.scrollTo(0, 0)
     }, [])
 
     useEffect(() => {
@@ -27,17 +27,14 @@ const Home = () => {
         const marginTopPx = logoStyle.getPropertyValue('margin-top')
         const marginTopInt = parseInt(marginTopPx, 10)
         const heightPx = logoStyle.getPropertyValue('height')
-        const heightInt = (parseInt(heightPx, 10))*0.736342
+        const heightInt = (parseInt(heightPx, 10)) * (620 / 842)
         const fullHeight = marginTopInt + heightInt
-        setTagHeight(fullHeight)
-    }, [])    
-
-    console.log(tagHeight);
-    
+        setHeadHeight(fullHeight)
+    }, [])
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
-            const headerTop = window.scrollY < tagHeight
+            const headerTop = window.scrollY < headHeight
             headerTop !== isTop
                 ?
                 setIsTop(false)
@@ -46,24 +43,20 @@ const Home = () => {
         })
     }, [])
 
-
-
-
-
-
-    const logoRef = React.createRef()
-
-
+    // style={isTop ? null : { marginBottom: `${headHeight}px` }}
 
     return (
         <div className='home'>
-            {/* <div className='header-container'>
-                <p ref={logoRef} className='header' style={{ top: `${tagHeight}px` }}>HEADER</p>
-            </div> */}
-            <div ref={logoRef} id='logoId' className={isTop ? 'logo-container' : 'logo-container2'}>
+            <div onClick={isTop ? null : () => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })} id='logoId' className={isTop ? 'logo-container' : 'logo-container2'}>
                 <figure className={`logo-figure ${shine}`} >
                     <img className={start} src={louCardWB} alt='logo' />
                 </figure>
+
+            </div>
+            <div className={`${isTop ? 'header2 header-invisible' : 'header header-visible'}`} >
+                <p className='navbar-item-underline'>PROJETS</p>
+                <p className='navbar-item-underline'>COLLECTION</p>
+                <p className='navbar-item-underline'>CONTACT</p>
             </div>
         </div>
     )
