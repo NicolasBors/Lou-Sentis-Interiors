c7dffd4578d565201853c13cd2683961d1ee4751
Nicolas Borson <nicolasborson@hotmail.fr>
2020-04-20 14:54:35 +0200

    assises and accessoires made

---
+++ src/components/collection/Accessoires.js
@@ -0,0 +1,408 @@
+import acc1 from '../../assets/pictures/accessoires/cintre1.JPG'
+import acc2 from '../../assets/pictures/accessoires/cintre2.JPG'
+import acc3 from '../../assets/pictures/accessoires/cintre3.JPG'
+import acc4 from '../../assets/pictures/accessoires/cintre4.JPG'
+import acc5 from '../../assets/pictures/accessoires/cintre5.JPG'
+import acc6 from '../../assets/pictures/accessoires/cintre6.JPG'
+
+import acc7 from '../../assets/pictures/accessoires/couss1.JPG'
+import acc8 from '../../assets/pictures/accessoires/couss2.JPG'
+import acc9 from '../../assets/pictures/accessoires/couss3.JPG'
+import acc10 from '../../assets/pictures/accessoires/couss4.JPG'
+import acc11 from '../../assets/pictures/accessoires/couss5.JPG'
+import acc12 from '../../assets/pictures/accessoires/couss6.JPG'
+import acc13 from '../../assets/pictures/accessoires/couss7.JPG'
+import acc14 from '../../assets/pictures/accessoires/couss8.JPG'
+import acc15 from '../../assets/pictures/accessoires/couss9.JPG'
+import acc16 from '../../assets/pictures/accessoires/couss10.JPG'
+import acc17 from '../../assets/pictures/accessoires/couss11.JPG'
+import acc18 from '../../assets/pictures/accessoires/couss12.JPG'
+
+import acc19 from '../../assets/pictures/accessoires/deco1.JPG'
+import acc20 from '../../assets/pictures/accessoires/deco2.JPG'
+import acc21 from '../../assets/pictures/accessoires/deco3.JPG'
+import acc22 from '../../assets/pictures/accessoires/deco4.JPG'
+import acc23 from '../../assets/pictures/accessoires/deco5.JPG'
+import acc24 from '../../assets/pictures/accessoires/deco6.JPG'
+import acc25 from '../../assets/pictures/accessoires/deco7.JPG'
+import acc26 from '../../assets/pictures/accessoires/deco8.JPG'
+import acc27 from '../../assets/pictures/accessoires/deco9.JPG'
+import acc28 from '../../assets/pictures/accessoires/deco10.JPG'
+import acc29 from '../../assets/pictures/accessoires/deco11.JPG'
+import acc30 from '../../assets/pictures/accessoires/deco12.JPG'
+import acc31 from '../../assets/pictures/accessoires/deco13.JPG'
+import acc32 from '../../assets/pictures/accessoires/deco14.JPG'
+import acc33 from '../../assets/pictures/accessoires/deco15.JPG'
+import acc34 from '../../assets/pictures/accessoires/deco16.JPG'
+import acc35 from '../../assets/pictures/accessoires/deco17.JPG'
+import acc36 from '../../assets/pictures/accessoires/deco18.JPG'
+import acc37 from '../../assets/pictures/accessoires/deco19.JPG'
+import acc38 from '../../assets/pictures/accessoires/deco20.JPG'
+
+import acc39 from '../../assets/pictures/accessoires/miroir1.JPG'
+import acc40 from '../../assets/pictures/accessoires/miroir2.JPG'
+
+import acc41 from '../../assets/pictures/accessoires/table1.JPG'
+import acc42 from '../../assets/pictures/accessoires/table2.JPG'
+import acc43 from '../../assets/pictures/accessoires/table3.JPG'
+import acc44 from '../../assets/pictures/accessoires/table4.JPG'
+import acc45 from '../../assets/pictures/accessoires/table5.JPG'
+import acc46 from '../../assets/pictures/accessoires/table6.JPG'
+import acc47 from '../../assets/pictures/accessoires/table7.JPG'
+
+import acc48 from '../../assets/pictures/accessoires/tapis1.JPG'
+import acc49 from '../../assets/pictures/accessoires/tapis2.JPG'
+import acc50 from '../../assets/pictures/accessoires/tapis3.JPG'
+
+import acc51 from '../../assets/pictures/accessoires/tissu1.JPG'
+import acc52 from '../../assets/pictures/accessoires/tissu2.JPG'
+import acc53 from '../../assets/pictures/accessoires/tissu3.JPG'
+import acc54 from '../../assets/pictures/accessoires/tissu4.JPG'
+
+import acc55 from '../../assets/pictures/accessoires/vase1.JPG'
+import acc56 from '../../assets/pictures/accessoires/vase2.JPG'
+import acc57 from '../../assets/pictures/accessoires/vase3.JPG'
+import acc58 from '../../assets/pictures/accessoires/vase4.JPG'
+import acc59 from '../../assets/pictures/accessoires/vase5.JPG'
+import acc60 from '../../assets/pictures/accessoires/vase6.JPG'
+import acc61 from '../../assets/pictures/accessoires/vase7.JPG'
+import acc62 from '../../assets/pictures/accessoires/vase8.JPG'
+import acc63 from '../../assets/pictures/accessoires/vase9.JPG'
+import acc64 from '../../assets/pictures/accessoires/vase10.JPG'
+import acc65 from '../../assets/pictures/accessoires/vase11.JPG'
+import acc66 from '../../assets/pictures/accessoires/vase12.JPG'
+import acc67 from '../../assets/pictures/accessoires/vase13.JPG'
+
+
+const Accessoires = {
+    title: 'Accessoires',
+    description: 'bla bla',
+    image: [
+        {
+            categorie: 'Portans',
+            caption: '',
+            src: acc1
+        },
+        {
+            categorie: 'Portants',
+            caption: '',
+            src: acc2
+        },
+        {
+            categorie: 'Portants',
+            caption: '',
+            src: acc3
+        },
+        {
+            categorie: 'Portants',
+            caption: '',
+            src: acc4
+        },
+        {
+            categorie: 'Portants',
+            caption: '',
+            src: acc5
+        },
+        {
+            categorie: 'Portants',
+            caption: '',
+            src: acc6
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc7
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc8
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc9
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc10
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc11
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc12
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc13
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc14
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc15
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc16
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc17
+        },
+        {
+            categorie: 'Coussins',
+            caption: '',
+            src: acc18
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc19
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc20
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc21
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc22
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc23
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc24
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc25
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc26
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc27
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc28
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc29
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc30
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc31
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc32
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc33
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc34
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc35
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc36
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc37
+        },
+        {
+            categorie: 'Décoration',
+            caption: '',
+            src: acc38
+        },
+        {
+            categorie: 'Miroirs',
+            caption: '',
+            src: acc39
+        },
+        {
+            categorie: 'Miroirs',
+            caption: '',
+            src: acc40
+        },
+        {
+            categorie: 'Arts de la table',
+            caption: '',
+            src: acc41
+        },
+        {
+            categorie: 'Arts de la table',
+            caption: '',
+            src: acc42
+        },
+        {
+            categorie: 'Arts de la table',
+            caption: '',
+            src: acc43
+        },
+        {
+            categorie: 'Arts de la table',
+            caption: '',
+            src: acc44
+        },
+        {
+            categorie: 'Arts de la table',
+            caption: '',
+            src: acc45
+        },
+        {
+            categorie: 'Arts de la table',
+            caption: '',
+            src: acc46
+        },
+        {
+            categorie: 'Arts de la table',
+            caption: '',
+            src: acc47
+        },
+        {
+            categorie: 'Tapis',
+            caption: '',
+            src: acc48
+        },
+        {
+            categorie: 'Tapis',
+            caption: '',
+            src: acc49
+        },
+        {
+            categorie: 'Tapis',
+            caption: '',
+            src: acc50
+        },
+        {
+            categorie: 'Tissus',
+            caption: '',
+            src: acc51
+        },
+        {
+            categorie: 'Tissus',
+            caption: '',
+            src: acc52
+        },
+        {
+            categorie: 'Tissus',
+            caption: '',
+            src: acc53
+        },
+        {
+            categorie: 'Tissus',
+            caption: '',
+            src: acc54
+        },
+        {
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc55
+        },
+        {
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc56
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc57
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc58
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc59
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc60
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc61
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc62
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc63
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc64
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc65
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc66
+        },{
+            categorie: 'Vases et plantes',
+            caption: '',
+            src: acc67
+        }
+    ]
+}
+
+export default Accessoires
---
+++ src/components/collection/Assises.js
@@ -0,0 +1,460 @@
+import canap1 from '../../assets/pictures/assises/canap1.JPG'
+import canap2 from '../../assets/pictures/assises/canap2.JPG'
+import canap3 from '../../assets/pictures/assises/canap3.JPG'
+import canap4 from '../../assets/pictures/assises/canap4.JPG'
+import canap5 from '../../assets/pictures/assises/canap5.JPG'
+import canap6 from '../../assets/pictures/assises/canap6.JPG'
+import canap7 from '../../assets/pictures/assises/canap7.JPG'
+import canap8 from '../../assets/pictures/assises/canap8.JPG'
+import canap9 from '../../assets/pictures/assises/canap9.JPG'
+import canap10 from '../../assets/pictures/assises/canap10.JPG'
+import canap11 from '../../assets/pictures/assises/canap11.JPG'
+import canap12 from '../../assets/pictures/assises/canap12.JPG'
+import canap13 from '../../assets/pictures/assises/canap13.JPG'
+import canap14 from '../../assets/pictures/assises/canap14.JPG'
+import canap15 from '../../assets/pictures/assises/canap15.JPG'
+import canap16 from '../../assets/pictures/assises/canap16.JPG'
+import canap17 from '../../assets/pictures/assises/canap17.JPG'
+import canap18 from '../../assets/pictures/assises/canap18.JPG'
+import canap19 from '../../assets/pictures/assises/canap19.JPG'
+import canap20 from '../../assets/pictures/assises/canap20.JPG'
+import canap21 from '../../assets/pictures/assises/canap21.JPG'
+import canap22 from '../../assets/pictures/assises/canap22.JPG'
+import canap23 from '../../assets/pictures/assises/canap23.JPG'
+import canap24 from '../../assets/pictures/assises/canap24.JPG'
+import canap25 from '../../assets/pictures/assises/canap25.JPG'
+import canap26 from '../../assets/pictures/assises/canap26.JPG'
+import canap27 from '../../assets/pictures/assises/canap27.JPG'
+import canap28 from '../../assets/pictures/assises/canap28.JPG'
+import canap29 from '../../assets/pictures/assises/canap29.JPG'
+import canap30 from '../../assets/pictures/assises/canap31.JPG'
+import canap31 from '../../assets/pictures/assises/canap32.JPG'
+import canap32 from '../../assets/pictures/assises/canap33.JPG'
+import canap33 from '../../assets/pictures/assises/canap34.JPG'
+import canap34 from '../../assets/pictures/assises/canap36.JPG'
+import canap35 from '../../assets/pictures/assises/canap37.JPG'
+import canap36 from '../../assets/pictures/assises/canap38.JPG'
+
+import chaise1 from '../../assets/pictures/assises/chaise1.JPG'
+import chaise2 from '../../assets/pictures/assises/chaise2.JPG'
+import chaise3 from '../../assets/pictures/assises/chaise3.JPG'
+import chaise4 from '../../assets/pictures/assises/chaise4.JPG'
+import chaise5 from '../../assets/pictures/assises/chaise5.JPG'
+import chaise6 from '../../assets/pictures/assises/chaise6.JPG'
+import chaise7 from '../../assets/pictures/assises/chaise7.JPG'
+import chaise8 from '../../assets/pictures/assises/chaise8.JPG'
+import chaise9 from '../../assets/pictures/assises/chaise9.JPG'
+import chaise10 from '../../assets/pictures/assises/chaise10.JPG'
+import chaise11 from '../../assets/pictures/assises/chaise11.JPG'
+import chaise12 from '../../assets/pictures/assises/chaise12.JPG'
+import chaise13 from '../../assets/pictures/assises/chaise13.JPG'
+import chaise14 from '../../assets/pictures/assises/chaise14.JPG'
+import chaise15 from '../../assets/pictures/assises/chaise15.JPG'
+import chaise16 from '../../assets/pictures/assises/chaise16.JPG'
+import chaise17 from '../../assets/pictures/assises/chaise18.JPG'
+import chaise18 from '../../assets/pictures/assises/chaise19.JPG'
+import chaise19 from '../../assets/pictures/assises/chaise20.JPG'
+import chaise20 from '../../assets/pictures/assises/chaise21.JPG'
+import chaise21 from '../../assets/pictures/assises/chaise22.JPG'
+import chaise22 from '../../assets/pictures/assises/chaise24.JPG'
+import chaise23 from '../../assets/pictures/assises/chaise25.JPG'
+import chaise24 from '../../assets/pictures/assises/chaise26.JPG'
+import chaise25 from '../../assets/pictures/assises/chaise27.JPG'
+import chaise26 from '../../assets/pictures/assises/chaise28.JPG'
+import chaise27 from '../../assets/pictures/assises/chaise29.JPG'
+import chaise28 from '../../assets/pictures/assises/chaise30.JPG'
+import chaise29 from '../../assets/pictures/assises/chaise31.JPG'
+import chaise30 from '../../assets/pictures/assises/chaise32.JPG'
+import chaise31 from '../../assets/pictures/assises/chaise33.JPG'
+import chaise32 from '../../assets/pictures/assises/chaise34.JPG'
+import chaise33 from '../../assets/pictures/assises/chaise35.JPG'
+import chaise34 from '../../assets/pictures/assises/chaise36.JPG'
+
+import pouf1 from '../../assets/pictures/assises/pouf1.JPG'
+import pouf2 from '../../assets/pictures/assises/pouf2.JPG'
+import pouf3 from '../../assets/pictures/assises/pouf3.JPG'
+import pouf4 from '../../assets/pictures/assises/pouf4.JPG'
+import pouf5 from '../../assets/pictures/assises/pouf5.JPG'
+import pouf6 from '../../assets/pictures/assises/pouf6.JPG'
+import pouf7 from '../../assets/pictures/assises/pouf7.JPG'
+import pouf8 from '../../assets/pictures/assises/pouf8.JPG'
+import pouf9 from '../../assets/pictures/assises/pouf9.JPG'
+import pouf10 from '../../assets/pictures/assises/pouf10.JPG'
+
+
+const Assises = {
+    title: 'Assises',
+    description: 'bla bla',
+    image: [
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap1
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap2
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap3
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap4
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap5
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap6
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap7
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap8
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap9
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap10
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap11
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap12
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap13
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap14
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap15
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap16
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap17
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap18
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap19
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap20
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap21
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap22
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap23
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap24
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap25
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap26
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap27
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap28
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap29
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap30
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap31
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap32
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap33
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap34
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap35
+        },
+        {
+            categorie: 'Fauteuils et canapés',
+            caption: '',
+            src: canap36
+        },
+        {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise1
+        },
+        {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise2
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise3
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise4
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise5
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise6
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise7
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise8
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise9
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise10
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise11
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise12
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise13
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise14
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise15
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise16
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise17
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise18
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise19
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise20
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise21
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise22
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise23
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise24
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise25
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise26
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise27
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise28
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise29
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise30
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise31
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise32
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise33
+        }, {
+            categorie: 'Chaises',
+            caption: '',
+            src: chaise34
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf1
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf2
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf3
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf4
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf5
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf6
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf7
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf8
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf9
+        },
+        {
+            categorie: 'Poufs',
+            caption: '',
+            src: pouf10
+        }
+    ]
+}
+
+export default Assises
